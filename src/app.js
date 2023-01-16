29503dc2a986415879d7b55d9a2218871e11d694
Crisabel111 <cristinavalencia918@gmail.com>
2023-01-15 23:26:15 -0500

    cambios vista

--- src/app.js
+++ src/app.js
@@ -3,13 +3,14 @@ const path = require('path');
 
 const app = express ();
 
-const publicPath = path.resolve(__dirname,'./public');
-app.use(express.static(publicPath));
+//const publicPath = path.resolve(__dirname,'./public');
 
+//app.use(express.static(publicPath));
+app.use( express.static('public') );
 app.listen(3030, () => {
     console.log ('Servidor corriendo en el puerto 3030');
 });
 
 app.get ('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.js'));
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
